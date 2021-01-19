0a5a33a05ff4f76b0f80c83276f55064fe1fa351
monbenard14852 <monbernard14852@gmail.com>
2021-01-19 21:15:38 +0700

    แก้ varible

--- varible.c
+++ varible.c
@@ -8,8 +8,9 @@ void main(){
      // ทำระบบสมัครสมาชิก เก็บ username, email, password, phone
      //                        char[] char[] char[]      int
      //การบ้าน เพิ่ม 1.ชื่อ 2.นามสกุล 3.อายุ 4.น้ำหนัก 5.ส่วนสูง
-    char username[20], email[50], password[20], repassword[20], fristname[20], surname[20]; 
-    int phone, born, height;
+    char username[20], email[50], password[20], repassword[20], fristname[20], surname[20], phone[20]; 
+    int age;
+    float height;
      // ============== ตัวแปล =====================
 
      // ============== การรับข้อมูล =================
@@ -31,11 +32,11 @@ void main(){
     printf("your surname is:");
     scanf("%s",&surname);
 
-    printf("your born is:");
-    scanf("%s",&born);
+    printf("your age is:");
+    scanf("%d",&age);
 
     printf("your height is:");
-    scanf("%s",&height);
+    scanf("%f",&height);
 
     printf("your phone is:");
     scanf("%s",&phone);
@@ -46,13 +47,15 @@ void main(){
 
      // ============== การแสดงผลข้อมูล =============
      printf("Success \n");
-     printf("you username is: %s\n",&username);
-     printf("your email is: %s\n",&email);
-     printf("your fristname is: %s\n",&fristname);
-     printf("your surname is: %s\n",&surname);
-     printf("your born is: %s\n",&born);
-     printf("your height is: %s\n",&height);
-     printf("your phone is: %s\n",&phone);
+     printf("you username is: %s\n",username);
+     printf("your email is: %s\n",email);
+     printf("your password is: %s\n",password);
+     printf("your repassword is: %s\n",repassword);
+     printf("your fristname is: %s\n",fristname);
+     printf("your surname is: %s\n",surname);
+     printf("your age is: %d\n",age);
+     printf("your height is: %.2f\n",height);
+     printf("your phone is: %s\n",phone);
 
      // ============== การแสดงผลข้อมูล =============
      }
